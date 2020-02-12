0980402f16cd1e20551d2d8269626848c1362820
Jakub Serwatka <kubaserw@gmail.com>
2020-02-12 20:07:13 +0100

    basic camera screen and control added

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:substances_ocr_app/screens/loading.dart';
 import 'package:substances_ocr_app/screens/home.dart';
 import 'package:substances_ocr_app/screens/camera.dart';
 import 'package:substances_ocr_app/screens/history.dart';
 
+Future<void> main() async {
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/': (context) => Loading(),
-    '/main': (context) => Home(),
-    '/camera': (context) => Camera(),
-    '/history': (context) => History(),
-  },
-));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(MaterialApp(
+    initialRoute: '/camera',
+    routes: {
+      '/': (context) => Loading(),
+      '/main': (context) => HomeScreen(),
+      '/camera': (context) => CameraScreen(
+        camera: firstCamera,
+      ),
+      '/history': (context) => History(),
+    },
+  ));
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,73 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:substances_ocr_app/screens/camera.dart';
 
-class Home extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int _selectedPage = 0;
+
+  // create a dialog "add new substance" function
+  createAddDialog(BuildContext context){
+
+    TextEditingController customController = TextEditingController();
+
+    return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: TextField(
+            controller: customController,
+            decoration: InputDecoration(
+              labelText: "wpisz substancję",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          actions: <Widget>[
+            MaterialButton(
+              elevation: 5.0,
+              child: Text(
+                "dodaj",
+                style: TextStyle(
+                  fontSize: 17.0,
+                  color: Colors.green[600],
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // search bar to implement
+//  void _searchPressed() {
+//    setState(() {
+//      if (this._searchIcon.icon == Icons.search) {
+//        this._searchIcon = new Icon(Icons.close);
+//        this._appBarTitle = new TextField(
+//          controller: _filter,
+//          decoration: new InputDecoration(
+//              prefixIcon: new Icon(Icons.search),
+//              hintText: 'Search...'
+//          ),
+//        );
+//      } else {
+//        this._searchIcon = new Icon(Icons.search);
+//        this._appBarTitle = new Text('Search Example');
+//        filteredNames = names;
+//        _filter.clear();
+//      }
+//    });
+//  }
+
   @override
   Widget build(BuildContext context) {
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -75,23 +141,31 @@ class Home extends StatelessWidget {
       ),
       // ---- bottom ----
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedPage,
         backgroundColor: Colors.grey[100],
         fixedColor: Colors.teal,
-          items: [
-            BottomNavigationBarItem(
-              title: Text("Główna"),
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(
-              title: Text("Skanuj"),
-              icon: Icon(Icons.camera_alt),
-            ),
-            BottomNavigationBarItem(
-              title: Text("Dodaj"),
-              icon: Icon(Icons.add),
-            ),
-          ]
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Główna"),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Skanuj"),
+            icon: Icon(Icons.camera_alt),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Dodaj"),
+            icon: Icon(Icons.add),
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedPage = index;
+          });
+        },
       ),
     );
   }
 }
+
+
